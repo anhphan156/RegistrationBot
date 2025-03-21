4fbf0aa53549db3897ddc37e02bab18bc2fbca14
backspace <anh.phan156@protonmail.com>
2025-03-20 21:05:36 -0400

    trying cross building for arm (not working yet)

--- flake.nix
+++ flake.nix
@@ -24,14 +24,43 @@
           ];
         };
       in {
-        packages.default = pkgs.rustPlatform.buildRustPackage {
+        packages.default = let
+          armPkgs = pkgs.pkgsCross.aarch64-multiplatform;
+          openssl = armPkgs.openssl;
+          # rustPlatform = armPkgs.makeRustPlatform {
+          #   rustc = pkgs.rust-toolchain;
+          #   cargo = pkgs.rust-toolchain;
+          # };
+        in
+          armPkgs.rustPlatform.buildRustPackage {
+            name = "RegistrationBot";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+            # preBuild = ''
+            #   export OPENSSL_DIR="${openssl}"
+            #   export OPENSSL_LIB_DIR="${openssl}/lib"
+            #   export OPENSSL_INCLUDE_DIR="${openssl}/include"
+            #   export PKG_CONFIG_PATH="${openssl}/lib/pkgconfig"
+            #   export PKG_CONFIG_ALLOW_CROSS=1
+            # '';
+            nativeBuildInputs = with armPkgs; [
+              pkg-config
+            ];
+            buildInputs = with armPkgs; [
+              openssl
+            ];
+            # target = "aarch64-unknown-linux-gnu";
+            # cargoBuildFlags = ["--target=aarch64-unknown-linux-gnu"];
+          };
+
+        packages.static = pkgs.pkgsStatic.rustPlatform.buildRustPackage {
           name = "RegistrationBot";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
-          nativeBuildInputs = with pkgs; [
+          nativeBuildInputs = with pkgs.pkgsStatic; [
             pkg-config
           ];
-          buildInputs = with pkgs; [
+          buildInputs = with pkgs.pkgsStatic; [
             openssl
           ];
         };
